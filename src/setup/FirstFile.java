db64e5ed03a7e05c595ad4be9a8fb9e1bf399d2a
davidward01 <davidward.info@gmail.com>
2021-10-05 16:06:50 -0400

    Init commit.  Added setup package and first class.

---
+++ src/setup/FirstFile.java
@@ -0,0 +1,10 @@
+package setup;
+
+public class FirstFile {
+    public static void main(String[] args) {
+        // this is the first file
+
+        // lorem ipsum dolor fsafawrgbawrbaerbha
+
+    }
+}
